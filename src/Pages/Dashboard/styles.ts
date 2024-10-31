8fcc36e466f6be27d69e1a1d2f5bce8d0b58b7ec
Victor Correia <162618994+victordevz@users.noreply.github.com>
2024-10-31 19:50:06 -0300

    separação estilo
    denivan criou a estilização no index, eu fiz a separação e chamei o arquivo styles no index.

---
+++ src/Pages/Dashboard/styles.ts
@@ -0,0 +1,46 @@
+// styles.ts
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 100px;
+`;
+
+export const ContainerCards = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const TitleNotificacoes = styled.h1`
+  margin: 35px 150px;
+  color: #E1E1E1;
+  font-family: Poppins;
+  font-size: 40px;
+  font-weight: 600;
+`;
+
+export const ContainerChat = styled.div`
+  width: 134px;
+  height: 134px;
+  flex-shrink: 0;
+  display: flex;
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  right: 50px;
+  bottom: 50px;
+  background-color: #2D393D;
+  border-radius: 50%;
+
+  img {
+    display: flex;
+    width: 73px;
+    height: 73px;
+    padding: 0px 0px 0px 0.072px;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+  }
+`;
